6b4cc6c9df1ad9ce494097252a3fad3983660ffd
gutoomota <h2mario@gmail.com>
2017-05-04 14:55:48 -0300

    AlamManager

---
+++ AlarmManager/app/src/main/java/com/guto/alarmmanager/MainActivity.java
@@ -0,0 +1,50 @@
+package com.guto.alarmmanager;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AlarmManagerBroadcastReceiver alarm;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        alarm = new AlarmManagerBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    public void startRepeatingTimer(View view) {
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.SetAlarm(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void cancelRepeatingTimer(View view){
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.CancelAlarm(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onetimeTimer(View view){
+        Context context = this.getApplicationContext();
+        if(alarm != null){
+            alarm.setOnetimeTimer(context);
+        }else{
+            Toast.makeText(context, "Alarm is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
